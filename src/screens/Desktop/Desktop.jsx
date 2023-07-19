9da8e13dbabd8a9b3e45a26ca84581e80cad3394
Ilya <115468838+gudkovsky@users.noreply.github.com>
2023-07-19 16:23:22 +0300

    Initial commit

---
+++ src/screens/Desktop/Desktop.jsx
@@ -0,0 +1,146 @@
+import React from "react";
+import "./style.css";
+
+export const Desktop = () => {
+  return (
+    <div className="desktop">
+      <div className="overlap-wrapper">
+        <div className="overlap">
+          <div className="overlap-group">
+            <img
+              className="red-light"
+              alt="Red light"
+              src="https://generation-sessions.s3.amazonaws.com/d948c6ebd9c68be49a630fd78f17357e/img/red-light.png"
+            />
+            <img
+              className="purple-light"
+              alt="Purple light"
+              src="https://generation-sessions.s3.amazonaws.com/d948c6ebd9c68be49a630fd78f17357e/img/purple-light.png"
+            />
+            <img
+              className="red-ball"
+              alt="Red ball"
+              src="https://generation-sessions.s3.amazonaws.com/d948c6ebd9c68be49a630fd78f17357e/img/red-ball.png"
+            />
+            <img
+              className="purple-ball"
+              alt="Purple ball"
+              src="https://generation-sessions.s3.amazonaws.com/d948c6ebd9c68be49a630fd78f17357e/img/purple-ball.png"
+            />
+            <div className="element">
+              30 готовых
+              <br />
+              решений
+            </div>
+            <div className="text-wrapper">ВИДЖЕТЫ</div>
+            <div className="dashboard">DASHBOARD</div>
+            <div className="div">
+              с показателями
+              <br />
+              вашего бизнеса
+            </div>
+            <div className="element-2">35 ДНЕЙ</div>
+            <div className="CRM">
+              использования
+              <br />
+              CRM
+            </div>
+            <div className="skype">SKYPE АУДИТ</div>
+            <div className="CRM-2">
+              отдела продаж
+              <br />
+              и CRM системы
+            </div>
+            <img
+              className="img"
+              alt="Img"
+              src="https://generation-sessions.s3.amazonaws.com/d948c6ebd9c68be49a630fd78f17357e/img/-------------------------------------------@2x.png"
+            />
+            <h1 className="h-1">
+              Зарабатывайте
+              <br />
+              больше
+            </h1>
+            <div className="text-wrapper-2">с WELBEX</div>
+            <div className="text-wrapper-3">Развиваем и контролируем продажи за вас</div>
+            <header className="header">
+              <img
+                className="logo-welbex"
+                alt="Logo welbex"
+                src="https://generation-sessions.s3.amazonaws.com/d948c6ebd9c68be49a630fd78f17357e/img/logo-welbex.svg"
+              />
+              <p className="p">крупный интегратор CRM в Росcии и ещё 8 странах</p>
+              <div className="text-wrapper-4">+7 555 555-55-55</div>
+              <img
+                className="whatsapp"
+                alt="Whatsapp"
+                src="https://generation-sessions.s3.amazonaws.com/d948c6ebd9c68be49a630fd78f17357e/img/whatsapp-1.svg"
+              />
+              <img
+                className="telegram"
+                alt="Telegram"
+                src="https://generation-sessions.s3.amazonaws.com/d948c6ebd9c68be49a630fd78f17357e/img/telegram-1.svg"
+              />
+              <img
+                className="viber"
+                alt="Viber"
+                src="https://generation-sessions.s3.amazonaws.com/d948c6ebd9c68be49a630fd78f17357e/img/viber-1.svg"
+              />
+              <div className="navbar">
+                <div className="text-wrapper-5">Виджеты</div>
+                <div className="text-wrapper-6">Интеграции</div>
+                <div className="text-wrapper-7">Кейсы</div>
+                <div className="text-wrapper-8">Сертификаты</div>
+                <div className="text-wrapper-9">Услуги</div>
+              </div>
+            </header>
+            <footer className="footer">
+              <p className="text-wrapper-10">Москва, Путевой проезд 3с1, к 902</p>
+              <div className="text-wrapper-11">+7 555 555-55-55</div>
+              <div className="text-wrapper-12">О КОМПАНИИ</div>
+              <div className="text-wrapper-13">МЕНЮ</div>
+              <div className="text-wrapper-14">КОНТАКТЫ</div>
+              <img
+                className="whatsapp-2"
+                alt="Whatsapp"
+                src="https://generation-sessions.s3.amazonaws.com/d948c6ebd9c68be49a630fd78f17357e/img/whatsapp-1.svg"
+              />
+              <img
+                className="telegram-2"
+                alt="Telegram"
+                src="https://generation-sessions.s3.amazonaws.com/d948c6ebd9c68be49a630fd78f17357e/img/telegram.svg"
+              />
+              <img
+                className="viber-2"
+                alt="Viber"
+                src="https://generation-sessions.s3.amazonaws.com/d948c6ebd9c68be49a630fd78f17357e/img/viber-1.svg"
+              />
+              <div className="text-wrapper-15">Партнёрская программа</div>
+              <div className="text-wrapper-16">Расчёт стоимости</div>
+              <div className="text-wrapper-17">Блог на Youtube</div>
+              <div className="text-wrapper-18">Услуги</div>
+              <div className="text-wrapper-19">Вопрос / Ответ</div>
+              <div className="text-wrapper-20">Виджеты</div>
+              <div className="text-wrapper-21">Интеграции</div>
+              <div className="text-wrapper-22">Наши клиенты</div>
+              <div className="text-wrapper-23">Кейсы</div>
+              <div className="text-wrapper-24">Благодарственные письма</div>
+              <div className="text-wrapper-25">Сертификаты</div>
+              <div className="text-wrapper-26">Вакансии</div>
+              <p className="text-wrapper-27">©WELBEX 2022. Все права защищены.</p>
+              <div className="text-wrapper-28">Политика конфиденциальности</div>
+            </footer>
+          </div>
+          <img
+            className="red-ball-2"
+            alt="Red ball"
+            src="https://generation-sessions.s3.amazonaws.com/d948c6ebd9c68be49a630fd78f17357e/img/red-ball-1.png"
+          />
+          <div className="btn">
+            <div className="text-wrapper-29">Получить консультацию</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
